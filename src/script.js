26d44edffee38f8c1f327c0e842a4401f828781c
Ramiro Vargas Mamani <ramiritogatito587@gmail.com>
2021-10-02 23:52:29 -0400

    commit 1

---
+++ src/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const buttonPoll = document.querySelector(".poll");
+
+// SU CODIGO EMPIEZA DESDE AQUI
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+    answers: new Array(4).fill(0),
+    };
+    registerNewAnswer: prompt
+    ("¿Cual es tu lenguaje de programacion favorito?, 0: JavaScript, 1: Python, 2: Kotiln, 3: C++", "Escriba la opcion que guste" ),
+console.log(poll.registerNewAnswer);
+
+// 1.- metodo registerNewAnswer
+// const registerNewAnswer = 
+// poll[question.split]('');
+// console.log(registerNewAnswer);
